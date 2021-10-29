54423615993b36990aa579566c7bbeba4ba3d2a4
Nicklas Osmers <nosmers@qass.net>
2021-10-29 09:10:42 +0200

    Added QTimer to Event Handler

--- src/event.py
+++ src/event.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+from PySide2.QtCore import QTimer, QObject
 
 
 class EventTypes:
@@ -8,7 +9,12 @@ class EventTypes:
 
 class EventHandler:
 	def __init__(self):
+		# super().__init__(parent = None)
 		self._subscribers = defaultdict(list) 
+		self._event_schedule = set()
+		self._fast_timer, self._interval_timer = QTimer(), QTimer()
+		self._interval_timer.timeout.connect(self._emit_events)
+		self._interval_timer.start(1000)
 
 	def register(self, event_type, func):
 		self._subscribers[event_type].append(func)
@@ -16,3 +22,12 @@ class EventHandler:
 	def emit(self, event_type):
 		for subscriber_function in self._subscribers.get(event_type, []):
 			subscriber_function()
+
+	def _emit_events(self):
+		for event in self._event_schedule:
+			for subscriber_function in self._subscribers.get(event, []):
+				subscriber_function()
+		self._event_schedule.clear()
+
+	def schedule(self, event_type):
+		self._event_schedule.add(event_type)
