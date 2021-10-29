6253da95ce9a672910c7fe1921839e93fbbf5ce3
Nicklas Osmers <nosmers@qass.net>
2021-10-29 09:40:50 +0200

    Added second Timer that restarts after each event schedule operation

--- src/event.py
+++ src/event.py
@@ -11,13 +11,30 @@ class EventHandler:
 	"""This class handles all the events that can be emitted when changing the configuratio
 	or content of the plot or anything in general. You can subscribe to a specific event
 	with a function and the Event Handler will execute that function whenever the event
-	gets emitted."""
-	def __init__(self):
+	gets emitted.
+	
+	:param variable_timer_interval: This timer is being resetted every time an event comes in
+	to prevent the plot from updating too often. This functionality will group events together
+	but allow single events to be handled faster.
+	:type variable_timer_interval: int, optional
+	:param set_timer_interval: The set timer will handle all events after each timeout to 
+	make sure the plot is updating even though a constant flow of events arrives and resets the 
+	variable_timer each time.
+	:type set_timer_interval: int, optional
+	"""
+	def __init__(self, variable_timer_interval = 20, set_timer_interval = 100):
 		self._subscribers = defaultdict(list) 
 		self._event_schedule = set()
-		self._fast_timer, self._interval_timer = QTimer(), QTimer()
-		self._interval_timer.timeout.connect(self._emit_events)
-		self._interval_timer.start(5000)
+		self._variable_timer_interval = variable_timer_interval
+		self._set_timer_interval = set_timer_interval
+		self._variable_timer, self._set_timer = QTimer(), QTimer()
+		self._init_timers()
+		
+	def _init_timers(self):
+		self._variable_timer.timeout.connect(self._emit_events)
+		self._set_timer.timeout.connect(self._emit_events)
+		self._set_timer.start(self._set_timer_interval)
+		
 
 	def register(self, event_type, func):
 		"""Register a function to the Event Handler. This function will be called whenever
@@ -37,6 +54,10 @@ class EventHandler:
 		for event in self._event_schedule:
 			self.emit(event)
 		self._event_schedule.clear()
+		# stop the variable timer after all events have been handled to reduce overhead
+		self._variable_timer.stop()
 
 	def schedule(self, event_type):
 		self._event_schedule.add(event_type)
+		# Start the variable timer whenever an event comes in
+		self._variable_timer.start(self._variable_timer_interval)
