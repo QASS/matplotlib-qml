68f0c8aed5ed6e5a2f824695cf3045da70be4704
Nicklas Osmers <nosmers@qass.net>
2021-10-29 09:19:53 +0200

    Added docstrings and cleaned up duplicate code

--- src/event.py
+++ src/event.py
@@ -8,25 +8,34 @@ class EventTypes:
 	FIGURE_DATA_CHANGED = "FIGURE_DATA_CHANGED"
 
 class EventHandler:
+	"""This class handles all the events that can be emitted when changing the configuratio
+	or content of the plot or anything in general. You can subscribe to a specific event
+	with a function and the Event Handler will execute that function whenever the event
+	gets emitted."""
 	def __init__(self):
-		# super().__init__(parent = None)
 		self._subscribers = defaultdict(list) 
 		self._event_schedule = set()
 		self._fast_timer, self._interval_timer = QTimer(), QTimer()
 		self._interval_timer.timeout.connect(self._emit_events)
-		self._interval_timer.start(1000)
+		self._interval_timer.start(5000)
 
 	def register(self, event_type, func):
+		"""Register a function to the Event Handler. This function will be called whenever
+		the corresponding event is emitted"""
 		self._subscribers[event_type].append(func)
 
 	def emit(self, event_type):
+		"""Emit an event directly (synchronously) without waiting for the next update interval
+		
+		:param event_type: :class:`EventTypes` constant describing the type of Event
+		:type event_type: str
+		"""
 		for subscriber_function in self._subscribers.get(event_type, []):
 			subscriber_function()
 
 	def _emit_events(self):
 		for event in self._event_schedule:
-			for subscriber_function in self._subscribers.get(event, []):
-				subscriber_function()
+			self.emit(event)
 		self._event_schedule.clear()
 
 	def schedule(self, event_type):
