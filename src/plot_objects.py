5bb2c2893199539045b790e2cd57a75b1f125fff
nosmers <nosmers@qass.net>
2021-11-15 08:34:26 +0100

    Added Scale Dictionary containing available Scales and introduced new property for scales

--- src/plot_objects.py
+++ src/plot_objects.py
@@ -9,6 +9,7 @@ from matplotlib_backend_qtquick.backend_qtquick import (
 from matplotlib_backend_qtquick.backend_qtquickagg import (
     FigureCanvasQtQuickAgg)
 from matplotlib.ticker import AutoLocator
+from matplotlib.scale import LinearScale, LogScale, SymmetricalLogScale, LogitScale, FuncScale
 from event import EventHandler, EventTypes
 from copy import copy
 
@@ -222,10 +223,14 @@ class Plot(QQuickItem):
 
 class Axis(QQuickItem):
     """Wrapper for matplotlib.pyplot.Axes"""
+
+    SCALE = {"linear" : "linear", "log" : "log", "symlog" : "symlog", "logit" : "logit"}
+
     def __init__(self, parent = None):
         super().__init__(parent)
         self._ax = None
         self._event_handler = None
+        self._scale = "linear"
         self._projection = "rectilinear"
         self._polar = False
         self._sharex = False
@@ -291,6 +296,7 @@ class Axis(QQuickItem):
         if self._grid:
             self._ax.grid(color = self._grid_color, linestyle = self._grid_linestyle, 
             linewidth = self._grid_linewidth, alpha = self._grid_alpha)
+        self._ax.set_xscale(self._scale)
         self._ax.set_axisbelow(True)
         self._ax.set_xlabel(self._x_axis_label, fontsize = self._x_axis_label_fontsize)
         self._ax.tick_params(axis = "x", colors = self._x_axis_tick_color)
@@ -472,6 +478,15 @@ class Axis(QQuickItem):
         self._y_axis_major_ticks = None
         self._y_axis_minor_ticks = None
         self._event_handler.schedule(EventTypes.AXIS_DATA_CHANGED)
+
+    def get_scale(self):
+        return self._scale
+
+    def set_scale(self, scale):
+        """Check if the provided scale is valid and provide linear as fallback if necessary"""
+        self._scale = self.SCALE.get(scale, "linear")
+        if self._ax is not None:
+            self._event_handler.schedule(EventTypes.AXIS_DATA_CHANGED)
 
     def get_projection(self):
         return self._projection
@@ -698,6 +713,7 @@ class Axis(QQuickItem):
             self._ax.set_ylim(*self._xlim, auto = None)
             self._event_handler.schedule(EventTypes.AXIS_DATA_CHANGED)
 
+    scale = Property(str, get_scale, set_scale)
     projection = Property(str, get_projection, set_projection) 
     polar = Property(bool, get_polar, set_polar)
     sharex = Property(bool, get_sharex, set_sharex)
